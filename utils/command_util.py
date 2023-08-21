364908db1f950a233b3e7395aec24003fa6dc54a
tigranb2 <tigran@Tigrans-Laptop.local>
2023-08-21 16:12:00 -0400

    fixed typo

--- utils/command_util.py
+++ utils/command_util.py
@@ -48,7 +48,7 @@ def get_server_cmd(config, timestamp, server_names_to_ips, server_name):
     if config['replication_protocol'] == "gryff":
         path_to_server_bin = os.path.join(config['remote_bin_directory'], 'gryff', 'server')
     elif config['replication_protocol'] == "pineapple":
-        path_to_master_bin = os.path.join(config['remote_bin_directory'], 'pineapple', 'server')
+        path_to_server_bin = os.path.join(config['remote_bin_directory'], 'pineapple', 'server')
     else:
         path_to_server_bin = os.path.join(config['remote_bin_directory'], 'gus-epaxos', 'server')
     server_addr = server_names_to_ips[server_name]
@@ -101,7 +101,7 @@ def get_client_cmd(config, timestamp, server_names_to_ips, server_id):
     if config['replication_protocol'] == "gryff":
         path_to_client_bin = os.path.join(config['remote_bin_directory'], 'gryff', 'client')
     elif config['replication_protocol'] == "pineapple":
-        path_to_master_bin = os.path.join(config['remote_bin_directory'], 'pineapple', 'client')
+        path_to_client_bin = os.path.join(config['remote_bin_directory'], 'pineapple', 'client')
 
     else:
         path_to_client_bin = os.path.join(config['remote_bin_directory'], 'gus-epaxos', 'client')
