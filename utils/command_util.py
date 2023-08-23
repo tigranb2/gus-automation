2524c5b88b082a0c6b850caa67a7c4af1b4253d7
tigranb2 <tigran@Tigrans-Laptop.local>
2023-08-22 22:33:16 -0400

    reverted changes

--- utils/command_util.py
+++ utils/command_util.py
@@ -137,8 +137,8 @@ def get_client_cmd(config, timestamp, server_names_to_ips, server_id):
     # Run client in the experiment directory.
     client_command = "cd %s && %s" % (exp_directory, client_command)
 
-    # stdout_file = os.#path.join(exp_directory, 'client-stdout.log')
-    # stderr_file = os.path.join(exp_directory, 'client-stderr.log')
-    #client_command = tcsh_redirect_output_to_files(client_command,
-    #                                               'client-stdout.log', 'client-stderr.log')
+    stdout_file = os.#path.join(exp_directory, 'client-stdout.log')
+    stderr_file = os.path.join(exp_directory, 'client-stderr.log')
+    client_command = tcsh_redirect_output_to_files(client_command,
+                                               'client-stdout.log', 'client-stderr.log')
     return client_command
