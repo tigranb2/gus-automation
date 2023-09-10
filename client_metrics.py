716fc5be03c3389b3aabddbb6f47c308ac04eb6a
Tigran Bantikyan <tigranbantikyan@gmail.com>
2023-09-09 22:04:47 -0500

    removed debug logs

--- client_metrics.py
+++ client_metrics.py
@@ -193,7 +193,6 @@ def results_data_to_metrics(options, results_data):
                     total_protocol_data = np.concatenate([total_protocol_data, file_contents])
 
             if "pineapple" in protocol or "pqr" in protocol:
-                print("this one ", protocol)
                 metrics[fig][protocol]["tput"] = {}
                 for file_key, _ in protocol_val.copy().items():
                     if "tput" in file_key:
