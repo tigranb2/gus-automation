58f0ed82227f6de87640faaddf9b9b332f001ed0
Tigran Bantikyan <tigranbantikyan@gmail.com>
2023-09-09 22:10:52 -0500

    debugging

--- client_metrics.py
+++ client_metrics.py
@@ -221,7 +221,7 @@ def metrics_table(metrics):
 
 # Deals with printing and saving to files
 def output_metrics(options, metrics):
-    print(metrics)
+    print("these", metrics)
     if "table" in options or "txt" in options:
         table = metrics_table(metrics)
 
