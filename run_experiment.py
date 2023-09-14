d786ab06816c0c0291d31cbf7582041d261453a1
Tigran Bantikyan <tigranbantikyan@gmail.com>
2023-09-14 18:08:24 -0500

    modified epaxos support

--- run_experiment.py
+++ run_experiment.py
@@ -143,7 +143,7 @@ def run_standard_experiment(server_names_to_internal_ips, config, timestamp, exe
     server_threads = start_servers(config, timestamp, server_names_to_internal_ips)
     client_threads = start_clients(config, timestamp, server_names_to_internal_ips)
 
-    if config['replication_protocol'] == "gryff" or config['replication_protocol'] == "epaxos":
+    if config['replication_protocol'] == "gryff":
         print('waiting for client to finish')
         client_threads.wait()
     else:
