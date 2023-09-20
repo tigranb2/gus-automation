a7cf0dc6aa2a665b8d925a5c3ec904bc9bc0cea5
Tigran Bantikyan <tigranbantikyan@gmail.com>
2023-09-19 23:55:17 -0500

    removed debug log

--- client_metrics.py
+++ client_metrics.py
@@ -154,7 +154,6 @@ def build_results_data(options):
                         else:
                             results_data[fig][protocol]["MAX"] = np.concatenate([results_data[fig][protocol]["MAX"], file_contents])
                         del results_data[fig][protocol][file_key]
-                print(results_data[fig][protocol]["MAX"])
 
     return results_data
 
