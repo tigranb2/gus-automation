0668fda065324e713e4d2345f05f15c8000baa5c
Tigran Bantikyan <tigranbantikyan@gmail.com>
2023-12-20 22:23:35 -0500

    updated compilation script

--- compile.sh
+++ compile.sh
@@ -2,30 +2,36 @@ if [ "$1" = "open-loop-client" ]; then
   cd ../pineapple
   git stash
   git checkout open-loop-client
+  git pull
   . compile.sh
 
   cd ../gus-epaxos
   git stash
   git checkout open-loop-epaxos
+  git pull
   . compile.sh
 
   cd ../gryff-testing
   git stash
   git checkout open-loop-client
+  git pull
   . compile.sh
 else
   cd ../pineapple
   git stash
   git checkout main
+  git pull
   . compile.sh
 
   cd ../gus-epaxos
   git stash
   git checkout main
+  git pull
   . compile.sh
 
   cd ../gryff-testing
   git stash
   git checkout main
+  git pull
   . compile.sh
 fi
